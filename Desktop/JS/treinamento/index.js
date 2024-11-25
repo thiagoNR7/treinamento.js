c413780bf686b7f12c32e249f6433fb5e0d29466
Thiago NR <thiagokroos55gmail.com>
2024-11-24 23:38:47 -0300

    treinamento

--- Desktop/JS/treinamento/index.js
+++ Desktop/JS/treinamento/index.js
@@ -1,3 +1,12 @@
-lista = [
-    {}
-]
+let lista = {
+    'acorda 9h30 para ir jogar bola': true, 
+    'ir joga bola': true, 
+    'fazer um gol': false, 
+    'da assistencia': false,
+    'corinthians ganhar': true,
+}
+
+
+for(let chave in lista){
+ console.log(`A tarefa: ${chave} está ${lista[chave] ? 'completa' : 'incompleta'}`)
+}
